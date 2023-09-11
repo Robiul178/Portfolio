298684d1a80771640ff4f8032e5f744ba87a781a
Robiul Islam <robiulislam.me2022@gmail.com>
2023-09-11 22:05:26 +0600

    add text

--- src/components/Home/Showcase/ShowCase.js
+++ src/components/Home/Showcase/ShowCase.js
@@ -60,6 +60,43 @@ const ShowCase = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="card card-compact w-72 md:w-96 bg-base-100 shadow-xl mb-5">
+                        <figure><img src={chamber} alt="Shoes" /></figure>
+                        <div className="card-body text-clip ">
+                            <h2 className="text-2xl font-bold text-black text-center">HolidayEscape</h2>
+                            <p
+                                className='text-gray-600 text-center text-lg'
+                            >
+                                A full stack responsive Healthcare service provider website
+                            </p>
+                            <div className="card-actions justify-end border-b-2 p-2">
+                                <a href="https://holidayescape-d354d.web.app/" target={0}>
+                                    <button className="btn btn-outline btn-info">
+                                        <FiMonitor></FiMonitor>
+                                        Live</button>
+                                </a>
+                                <a href="https://github.com/Robiul178/doctor-portal" target={0}>
+                                    <button className="btn btn-outline btn-success">
+                                        <FiGithub></FiGithub>
+                                        Client</button>
+                                </a>
+                                <a href="https://github.com/Robiul178/doctor-portl-server" target={0}>
+                                    <button className="btn btn-outline btn-warning">
+                                        <FiGithub></FiGithub>
+                                        server</button>
+                                </a>
+                            </div>
+                            <div>
+                                <p className='text-black text-md text-md'>
+                                    <span className=" text-lg font-bold">
+                                        Technologies:
+                                    </span> <br />
+                                    ReactJs, NodeJs, ExpressJs, MongoDB,TailwindCSS ,Firebase.
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
