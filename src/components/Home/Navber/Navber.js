5c6b8cf50d6b8a91ab44ab46b352adc247ae028d
Robiul Islam <robiulislam.me2022@gmail.com>
2023-09-21 21:56:25 +0600

    change

--- src/components/Home/Navber/Navber.js
+++ src/components/Home/Navber/Navber.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../App.css'
 import { Link as NavLink } from 'react-scroll'
-import resume from "../../../assets/Robiul's Resume.pdf"
+import resume from "../../../assets/robiulIslam.pdf"
 import { Link } from 'react-router-dom';
 
 const Navber = () => {
